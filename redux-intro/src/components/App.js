37afb9100e6067a426fe464a2b51d8184a755345
Liz Goff <livingearthcreations@gmail.com>
2018-05-22 11:23:06 -0500

    added logger

--- redux-intro/src/components/App.js
+++ redux-intro/src/components/App.js
@@ -3,26 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 // allows you to connect with Redux and dispatch actions
 // stuff that connects and receives
+
+
+// don't forget to npm install redux and react-redux --save
+// it doesn't replace complete state it works with it.
 import { connect } from 'react-redux';
 
 
 
 // the handleClick activates the reducer
 class App extends Component {
-  handleClick = () => {
-    this.props.dispatch(
-      { type: 'BUTTON_ONE'}
-    )
+
+  handleClickOne = () => {
+    const action = { type: 'BUTTON_ONE', someProp: 'hello'};
+    this.props.dispatch(action)
   }
+
+  handleClickTwo = () => {
+    const action = { type: 'BUTTON_TWO', someProp: 'world'};
+    this.props.dispatch(action)
+  }
+
 
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick}>BUTTON 1</button>
+        <button onClick={this.handleClickOne}>BUTTON One</button>
+        <button onClick={this.handleClickTwo}>BUTTON Two</button>
       </div>
     );
   }
 }
+
+// can also be written this way
+// <div className='App'>
+// <button onClick{() => {this.props.dispatch({ type: 'BUTTON_ONE'})}>BUTTON One</button>
+// <div className='App'><button onClick{() => {this.props.dispatch({ type: 'BUTTON_TWO'})}>BUTTON Two</button>
 
 
 // this type of structure is called currying
--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -3,24 +3,69 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+// the store communicates and passes action information
+// middleware calls out before the reducer
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 // Provider makes the connection between redux and app
 import { Provider } from 'react-redux';
+// redux logs out useful info about redux so we don't have to
+import logger from 'redux-logger';
+
+
+// redux isn't necessary for every application. for a group project it is enough code to spend the effort to do this up front.
+
+
+// the action is what we dispatched from app.js or whatever component you'd like
+const firstReducer = (state, action) => {
+    if (action.type === 'BUTTON_ONE') {
+        console.log('firstReducer', action);
+    }
+    return {};
+}
+
+const secondReducer = (state, action) => {
+    if (action.type === 'BUTTON_TWO') {
+        console.log('secondReducer', action);
+    }
+    return {};
+}
+
 
 
 // createStore takes in a function which is just a reducer
 // a reducer is a function that runs whenever and action is dispatched
 // all an action is, is an object. It describes what happened.
+// the instance is a function that takes in a function
+// store can only take in one reducer
 const storeInstance = createStore(
-    () => {
-        console.log('I am a reducer!')
-    },
-)
+    // combines the reducer and creates extra validation of state
+    combineReducers({
+        // key and value
+        // firstReducer: firstReducer,
+
+        // this is a shortcut to get the same thing
+        firstReducer,
+        secondReducer
+    }), 
+    // this attaches the logs to the console log so that you can see its state and info.
+        applyMiddleware(logger),
+);
+
+// Use this to first in the above to make sure it is connected
+
+// () => {
+//     console.log('A store is a reducer!')
+// },
+
 
 // components start with Capital letters
 // variable are camel case as in lower case 
 // constants can be screaming snake case like PORT
 
- 
+// wrap the entire App in the Provider
+// passing the store into the provider gives acess to the provider to the store. 
+
+
+// shouldn't the firstReducer be in here too?
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
